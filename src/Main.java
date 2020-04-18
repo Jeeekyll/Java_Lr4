e7e297280c29965cdc021ed42ea17ac939588d25
Evgeniy <poddubniy209@gmail.com>
2020-04-18 15:15:24 +0300

    asdas

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         public static void main(String[] args) {
         Window window = new Window();
         System.out.println("Hello");
+        System.out.println("Git rrr");
             System.out.println("Git rrr");
 
     }
