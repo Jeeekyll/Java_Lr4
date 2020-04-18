0245dc5c559c34c77f42a9d0ce5e7aaf36542854
Evgeniy <poddubniy209@gmail.com>
2020-04-18 15:11:28 +0300

    hhhh

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         public static void main(String[] args) {
         Window window = new Window();
         System.out.println("Hello");
-            System.out.println("Git");
-        
+            System.out.println("Git rrr");
+
     }
 }
